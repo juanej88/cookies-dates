f57c65d5a2729d610252316edda4d43c029ec358
Juan Espinosa <arq.jorrin@gmail.com>
2024-06-21 23:15:59 +0800

    Create the Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,21 @@
 import '../assets/styles/Header.css';
 
 const Header = () => {
+  const options = {
+    weekday: 'short',
+    day: '2-digit',
+    month: 'short'
+  };
+  const date = new Date().toLocaleString(undefined, options);
+
   return(
     <header>
-      <h1>Cookies & Dates</h1>
+      <h1>Cookies<span>& Dates</span></h1>
+      <div className='date'><h3>{date}</h3></div>
+      <div className='user'>
+        <h3>Juan</h3>
+        <div className='cookie'></div>
+      </div>
     </header>
   );
 };
