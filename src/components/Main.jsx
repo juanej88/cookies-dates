d5abae2c6a593b31f87a89e1f31f3c04e725dc10
Juan Espinosa <arq.jorrin@gmail.com>
2024-06-24 17:30:50 +0800

    Create the Main component

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,27 @@
 import '../assets/styles/Main.css';
+import {events} from '../assets/data/data';
 
 const Main = () => {
-  return(
+  const options = {weekday: 'short', day: '2-digit', month: 'short'};
+
+  const eventCards = events.sort((a, b) => a.date - b.date)
+  .map(event => {
+    const eventDate = new Date(event.date).toLocaleString(undefined, options);
+    const eventType = `${event.type === 'event' ? 'event' : 'birthday'}`;
+    return (
+      <div key={event.id} className={`event-card ${eventType}`}>
+        <h2>{event.name}</h2>
+        <p>{eventDate}</p>
+      </div>
+    );
+  });
+
+  return (
     <main>
-      <h2>Main</h2>
+      <section className='events'>
+        <h1>Upcoming Events:</h1>
+        {eventCards}
+      </section>
     </main>
   );
 };
