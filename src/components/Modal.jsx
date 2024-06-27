030d27a6de9dcc4067290ac88236ce0f9581bab0
Juan Espinosa <arq.jorrin@gmail.com>
2024-06-27 15:34:18 +0800

    Add Name and Date input

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import '../assets/styles/Modal.css';
 
 const Modal = (props) => {
@@ -8,10 +9,29 @@ const Modal = (props) => {
     };
   };
 
+  const [formData, setFormData] = useState({
+    name: '',
+    date: '',
+  });
+
+  const updateFormData = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  let maxDate = new Date().toJSON();
+  maxDate = maxDate.slice(0, 10);
+
   return (
     <section id='date-section' onClick={handleExteriorClick}>
       <form id='date-form'>
         <h2>Add Date</h2>
+        <label htmlFor="name">Name</label>
+        <input type="text" name='name' id='name' minLength='2' maxLength='20' value={formData.name} onChange={updateFormData} required autoFocus />
+        <label htmlFor='date'>Date</label>
+        <input type='date' name='date' id='date' max={maxDate} value={formData.date} onChange={updateFormData}  required/>
       </form>
     </section>
   );
