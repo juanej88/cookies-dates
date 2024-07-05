52bcf5113a124dcd8dd97d5c706959aa0609712c
Juan Espinosa <arq.jorrin@gmail.com>
2024-07-05 23:59:34 +0800

    Design menuIcon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import '../assets/styles/Header.css';
+// import menuIcon1 from '../assets/icons/menuIcon1.svg';
 
 const Header = () => {
+  const [openMenu, setOpenMenu] = useState(false);
+  const handleClick = () => {
+    setOpenMenu(prevState => !prevState);
+  };
+
   return(
     <header>
       <p id='logo'>Cookies
@@ -8,7 +15,10 @@ const Header = () => {
       </p>
       <div className='user'>
         <p>Juan</p>
-        <span className='material-symbols-outlined'>cookie</span>
+        {/* <img id='menu-icon' src={menuIcon1} alt='menu icon' /> */}
+        <i id='menu-icon' className={openMenu ? 'menu-icon-active' : ''} onClick={handleClick}></i>
+
+        {/* <span className='material-symbols-outlined'>cookie</span> */}
       </div>
     </header>
   );
