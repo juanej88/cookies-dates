31d59f0bd9dcbfe18d07647247c8dfc1110bcce2
Juan Espinosa <arq.jorrin@gmail.com>
2024-08-04 15:50:07 +0800

    Style DeleteEvent component and add a delete button

--- src/components/DeleteEvent.jsx
+++ src/components/DeleteEvent.jsx
@@ -1,8 +1,21 @@
 import '../assets/styles/DeleteEvent.css';
 
 const DeleteEvent = props => { 
+  const getMessage = () => {
+    let endChar = props.data.name.split('');
+    endChar[endChar.length - 1] === 's' ? endChar = `'` :
+    endChar = `'s`;
+    return props.data.event === 'birthday' ?
+    <p>Are you sure you want to delete <strong>{props.data.name}{endChar}</strong> birthday?</p> :
+    <p>Are you sure you want to delete the event titled <strong>"{props.data.name}"</strong>?</p>
+  };
+
   return (
-    <p>Once an event is deleted, it cannot be recovered. Are you sure you want to delete {props.data.name}?</p>
+    <aside className='delete-message-container'>
+      <p><strong>Warning</strong>: Deleting this event is permanent and cannot be undone.</p>
+      {getMessage()}
+      <button type='submit'>Yes, delete it</button>
+    </aside>
   );
 };
 
