479c66a5d1c32ad1a8f9f4d5738cca57cb3a7ef2
Juan Espinosa <arq.jorrin@gmail.com>
2024-08-27 09:48:12 +0800

    Animate Loader component

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,37 @@
+import { useEffect, useState } from 'react';
 import '../assets/styles/Loader.css';
 
 const Loader = () => {
+  const [image, setImage] = useState(1);
+
+  useEffect(() => {
+    const swapImages = () => {
+      switch(image) {
+        case 1:
+          setImage(2);
+          break;
+        case 2:
+          setImage(3);
+          break;
+        case 3:
+          setImage(4);
+          break;
+        default:
+          setImage(1);
+      };
+    };
+
+    const imgInterval = setInterval(() => {
+      swapImages();
+    }, 500);
+
+    return () => clearInterval(imgInterval);
+  }, [image]);
+  
+  
   return (
     <section id='loader'>
-      <span className='loader-img'></span>
+      <span className={`loader-img loader-img-${image}`}></span>
     </section>
   );
 };
