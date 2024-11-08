3fd4a03bb944288a19a0e6ea3d7c23964b9b9459
Juan Espinosa <arq.jorrin@gmail.com>
2024-11-08 20:20:21 +0800

    Create 2 unit tests to check incorrect and correct inputs passed to the EventForm

--- src/components/__tests__/EventForm.test.js
+++ src/components/__tests__/EventForm.test.js
@@ -1,30 +1,73 @@
 import { act } from 'react';
-import { screen } from '@testing-library/react';
-import ReactDOMClient from 'react-dom/client';
+import { screen, render, fireEvent } from '@testing-library/react';
 import EventForm from '../EventForm';
 
-it('renders the EventForm component correctly', () => {
-  const formData = {
-    event_type: 'birthday',
-    name: '',
-    dateInput: '',
-    date: '',
-    notify: true,
-    notification_days: 0,
-    operation: 'add-event',
-  };
-  const setFormData = jest.fn();
+describe('Event Form', () => {
+  it('renders the EventForm component correctly', () => {
+    const formData = {
+      event_type: 'birthday',
+      name: '',
+      dateInput: '',
+      date: '',
+      notify: true,
+      notification_days: 0,
+      operation: 'add-event',
+    };
+    const setFormData = jest.fn();
+  
+    act(() => {
+      render(<EventForm formData={formData} setFormData={setFormData} btnTag={'Add'} type={'add'} />);
+    });
 
-  const container = document.createElement('div');
-  document.body.appendChild(container);
-
-  act(() => {
-    ReactDOMClient.createRoot(container).render(<EventForm formData={formData} setFormData={setFormData} />);
+    const submitButton = screen.getByRole('button', { name: /add/i });
+  
+    // Check if all form elements are rendered
+    expect(screen.getByRole('radio', { name: /birthday/i })).toBeChecked();
+    expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/date/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/notification/i)).toBeInTheDocument();
+    expect(submitButton).toBeDisabled();
   });
 
-  // Check if all form elements are rendered
-  expect(screen.getByRole('radio', { name: /birthday/i })).toBeChecked();
-  expect(screen.getByLabelText(/name/i)).toBeInTheDocument();
-  expect(screen.getByLabelText(/date/i)).toBeInTheDocument();
-  expect(screen.getByLabelText(/notification/i)).toBeInTheDocument();
+  it('shows an error message when the name input is incorrect', () => {
+    const formData = {
+      event_type: 'birthday',
+      name: 'John $mith',
+      dateInput: '31/01/2000',
+      date: '2000-01-31',
+      notify: true,
+      notification_days: 0,
+      operation: 'add-event',
+    };
+    const setFormData = jest.fn();
+  
+    act(() => {
+      render(<EventForm formData={formData} setFormData={setFormData} btnTag={'Add'} type={'add'} />);
+    });
+
+    // Check if the error message shows up
+    expect(screen.getByText(/the name contains invalid characters/i)).toBeInTheDocument();
+  });
+
+  it('enables the submit button when the inputs are correct', () => {
+    const formData = {
+      event_type: 'birthday',
+      name: 'John Smith',
+      dateInput: '31/01/2000',
+      date: '2000-01-31',
+      notify: true,
+      notification_days: 0,
+      operation: 'add-event',
+    };
+    const setFormData = jest.fn();
+  
+    act(() => {
+      render(<EventForm formData={formData} setFormData={setFormData} btnTag={'Add'} type={'add'} />);
+    });
+
+    const submitButton = screen.getByRole('button', { name: /add/i });
+
+    // Check if the submitButton is enabled
+    expect(submitButton).toBeEnabled();
+  });
 });
